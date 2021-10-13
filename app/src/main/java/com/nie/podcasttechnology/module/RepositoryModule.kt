72cb460eaf82bb4051903f40c02ba0e586de77dd
Nie <chengyinie@kkbox.com>
2021-10-13 16:58:46 +0800

    move repository path

--- app/src/main/java/com/nie/podcasttechnology/module/RepositoryModule.kt
+++ app/src/main/java/com/nie/podcasttechnology/module/RepositoryModule.kt
@@ -1,8 +1,12 @@
-package com.nie.podcasttechnology.repository
+package com.nie.podcasttechnology.module
 
 import com.nie.podcasttechnology.data.database.PodcastTechnologyDatabase
 import com.nie.podcasttechnology.data.database.dao.EpisodeDao
 import com.nie.podcasttechnology.data.remote.Api
+import com.nie.podcasttechnology.repository.DatabaseRepository
+import com.nie.podcasttechnology.repository.DatabaseRepositoryImpl
+import com.nie.podcasttechnology.repository.EpisodeListRepository
+import com.nie.podcasttechnology.repository.EpisodeListRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
